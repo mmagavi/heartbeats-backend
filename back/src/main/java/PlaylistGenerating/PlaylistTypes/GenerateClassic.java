e48ffb0ba7193ad219195392ce316098ebef8135
Dalton-Simonson <113248699+Dalton-Simonson@users.noreply.github.com>
2023-06-14 15:28:33 -0400

    hopefully done with warmuo

--- back/src/main/java/PlaylistGenerating/PlaylistTypes/GenerateClassic.java
+++ back/src/main/java/PlaylistGenerating/PlaylistTypes/GenerateClassic.java
@@ -32,6 +32,8 @@ public class GenerateClassic extends GeneratePlaylist {
     private int min_allowable_length_ms;
     private int max_allowable_length_ms;
 
+    HashMap<Integer, TrackSimplified[]> intervals;
+
     //TODO: Play around with limit and see how few songs we can fetch while still getting good results
     private final int limit = 21; // Number of tracks we want to get
 
@@ -53,6 +55,8 @@ public class GenerateClassic extends GeneratePlaylist {
 
         super(spotify_api, genres, age, workout_length, intensity);
 
+        intervals = new HashMap<>();
+
         target_bpm = getTargetBPM();
 
         // warmup and wind-down are the same length and are 10% of the workout each
@@ -71,12 +75,11 @@ public class GenerateClassic extends GeneratePlaylist {
 
         bpm_difference = findBpmDifference();
 
-        int warmup_wind_down_length_ms = (int) (warmup_wind_down_length * 60_000); // conversion
+        warmup_wind_down_length_ms = (int) (warmup_wind_down_length * 60_000); // conversion
 
         // MilliSeconds of length that is acceptable for the warmup / wind-down sequence based on our MOE
-        int min_allowable_length_ms = warmup_wind_down_length_ms - (warmup_wind_down_length_ms * margin_of_error);
-        int max_allowable_length_ms = warmup_wind_down_length_ms + (warmup_wind_down_length_ms * margin_of_error);
-
+        min_allowable_length_ms = warmup_wind_down_length_ms - (warmup_wind_down_length_ms * margin_of_error);
+        max_allowable_length_ms = warmup_wind_down_length_ms + (warmup_wind_down_length_ms * margin_of_error);
     }
 
     /**
@@ -135,7 +138,6 @@ public class GenerateClassic extends GeneratePlaylist {
                 // Due to input validation in GeneratePlaylistHandler this should not be reached
             }
         }
-
     }
 
     /**
@@ -250,28 +252,101 @@ public class GenerateClassic extends GeneratePlaylist {
     }
 
     /**
-     * @param intervals
      * @param is_warmup
      * @return
      * @throws Exception
      */
-    private String[] findOrdering(HashMap<Integer, TrackSimplified[]> intervals, boolean is_warmup) throws Exception {
+    private String[] findOrdering(boolean is_warmup) throws Exception {
 
-        boolean column_found = false; // flag for while loop
+        String[] track_ids = null;
         int closest_column;
 
         do {
-            intervals = getSortedIntervals(is_warmup); // move this out of function possibly
+            intervals = getSortedIntervals(is_warmup); // populated class variable intervals with sorted intervals
 
-            closest_column = findClosestColumn(intervals);
+            closest_column = findClosestColumn();
 
-        } while (closest_column == -1); // if the closest column was not found we will retry
+            if (closest_column == -1) continue; // a column close enough to the duration was not found so start again
 
-        TrackSimplified[] tracks = getTracksInColumn(intervals, closest_column);
+            track_ids = getBestFit(closest_column);
+
+        } while (track_ids == null); // if an acceptable ordering was not found, try again
+
+        return getBestFit(closest_column);
+    }
+
+    /**
+     * Now that the closest column of songs has been found, preform some fine grain searching to find a good duration
+     *
+     * @param closest_column closest column of songs that was found and provided by the calling function
+     * @return String array of the track IDS, or null if a good ordering could not be found
+     */
+    private String[] getBestFit(int closest_column) {
+
+        TrackSimplified[] tracks = getTracksInColumn(closest_column);
+        TrackSimplified[] adjacent_tracks; // Tracks that are shorter or longer than closest_column depending on result
+        TrackSimplified[][] track_matrix;
         DURATION_RESULT result = checkDuration(tracks);
 
-        if(result == DURATION_RESULT.ACCEPTABLE){
+        if (result == DURATION_RESULT.ACCEPTABLE) {
             return getTrackIDs(tracks);
+        } else if (result == DURATION_RESULT.TOO_SHORT || result == DURATION_RESULT.WITHIN_THIRTY_SECONDS_SHORT) {
+
+            if (closest_column < (limit - 1)) {
+                adjacent_tracks = getTracksInColumn(closest_column + 1);
+            } else return null; // if the longer tracks are out of bounds no combination will be suitable
+
+        } else { // Too long or within 30 seconds too long
+            if (closest_column > 0) {
+                adjacent_tracks = getTracksInColumn(closest_column - 1);
+            } else return null; // if the shorter tracks are out of bounds no combination will be suitable
+        }
+
+        track_matrix = new TrackSimplified[num_intervals][2];
+
+        // Populate the track matrix
+        for (int row = 0; row < num_intervals; row++) {
+            track_matrix[row][0] = tracks[row];
+            track_matrix[row][1] = adjacent_tracks[row];
+        }
+
+        tracks = tryTrackCombinations(track_matrix, 0);
+
+        return getTrackIDs(tracks);
+    }
+
+    /**
+     * @param matrix
+     * @param tracks
+     * @param row
+     * @return
+     */
+    private TrackSimplified[] tryTrackCombinations(TrackSimplified[][] matrix, TrackSimplified[] tracks, int row) {
+        DURATION_RESULT result;
+        TrackSimplified[] track_combo;
+
+        if (row == matrix.length) {
+
+            result = checkDuration(tracks);
+
+            if (result == DURATION_RESULT.ACCEPTABLE) {
+                return tracks;
+            } else {
+                return null;
+            }
+        }
+
+        // index < 2 as there is always 2 columns to analyze
+        for (int column = 0; column < 2; column++) {
+
+            tracks[row] = matrix[row][column];
+
+            track_combo = tryTrackCombinations(matrix, tracks, row + 1);
+
+            // if the track_combo is null it means the final track array was not acceptable 
+            if (track_combo != null) {
+                return track_combo;
+            }
         }
 
         return null;
@@ -280,17 +355,16 @@ public class GenerateClassic extends GeneratePlaylist {
     /**
      * Finds the column closest to the target duration
      *
-     * @param intervals intervals from which to find the best fitting column
      * @return the closest column or -1 if there was not a good column
      */
-    private int findClosestColumn(HashMap<Integer, TrackSimplified[]> intervals) {
+    private int findClosestColumn() {
         TrackSimplified[] current_tracks;
 
         // First check if the shortest combination is too long and check if the longest combination is too short
-        current_tracks = getTracksInColumn(intervals, 0);
+        current_tracks = getTracksInColumn(0);
         if (checkDuration(current_tracks) == DURATION_RESULT.TOO_LONG) return -1;
 
-        current_tracks = getTracksInColumn(intervals, limit - 1);
+        current_tracks = getTracksInColumn(limit - 1);
         if (checkDuration(current_tracks) == DURATION_RESULT.TOO_SHORT) return -1;
 
         int current_column = limit / 2; // middle column
@@ -302,7 +376,7 @@ public class GenerateClassic extends GeneratePlaylist {
 
         while (distance_to_previous_column >= 4 && distance_to_bound >= 3) {
 
-            current_tracks = getTracksInColumn(intervals, current_column);
+            current_tracks = getTracksInColumn(current_column);
             result = checkDuration(current_tracks);
 
             if (result == DURATION_RESULT.TOO_SHORT) {
@@ -310,14 +384,12 @@ public class GenerateClassic extends GeneratePlaylist {
 
                 distance_to_previous_column = new_column - current_column; // new - current since new will be greater
                 distance_to_bound = (limit - 1) - new_column; // distance to right side of TrackSimplified array
-            }
-            else if (result == DURATION_RESULT.TOO_LONG) {
+            } else if (result == DURATION_RESULT.TOO_LONG) {
                 new_column = (current_column - 1) - (current_scope / 2); // mid-point of the left side of the current
 
                 distance_to_previous_column = current_column - new_column; // current - new since current is greater
                 distance_to_bound = new_column; // distance from 0
-            }
-            else { // In this case the column is either an acceptable length or 30 seconds off from the target
+            } else { // In this case the column is either an acceptable length or 30 seconds off from the target
                 break;
             }
 
@@ -327,15 +399,13 @@ public class GenerateClassic extends GeneratePlaylist {
         // In the cases that we left the while loop due to being within 30 seconds of the allowable duration, or we
         // got too close to the bounds of the track arrays we are going to search adjacent columns for best fit
 
-        if(result == DURATION_RESULT.TOO_SHORT || result == DURATION_RESULT.WITHIN_THIRTY_SECONDS_SHORT){
-            return checkForLongerColumn(intervals, current_column);
-        }
-        else if(result == DURATION_RESULT.TOO_LONG || result == DURATION_RESULT.WITHIN_THIRTY_SECONDS_LONG){
-            return checkForShorterColumn(intervals, current_column);
+        if (result == DURATION_RESULT.TOO_SHORT || result == DURATION_RESULT.WITHIN_THIRTY_SECONDS_SHORT) {
+            return checkForLongerColumn(current_column);
+        } else if (result == DURATION_RESULT.TOO_LONG || result == DURATION_RESULT.WITHIN_THIRTY_SECONDS_LONG) {
+            return checkForShorterColumn(current_column);
         } else if (result == DURATION_RESULT.ACCEPTABLE) {
             return current_column;
-        }
-        else {
+        } else {
             return -1;
         }
     }
@@ -343,81 +413,78 @@ public class GenerateClassic extends GeneratePlaylist {
     /**
      * Checks columns to the right of the given column in the TrackSimplified arrays in the intervals map for better
      * fitting columns
-     * @param intervals map to search the TrackSimplified arrays of
+     *
      * @param column column to start from
      * @return best fitting column
      */
-    private int checkForLongerColumn(HashMap<Integer, TrackSimplified[]> intervals, int column){
+    private int checkForLongerColumn(int column) {
 
         TrackSimplified[] next_column_tracks;
         DURATION_RESULT result;
 
-        do{
+        do {
 
             // if the next column is out of bounds return the current column
-            if(column + 1 >= limit) return column;
+            if (column + 1 >= limit) return column;
 
-            next_column_tracks = getTracksInColumn(intervals, column + 1);
+            next_column_tracks = getTracksInColumn(column + 1);
 
             result = checkDuration(next_column_tracks);
 
             // if the next column is a good fit, return it
-            if(result == DURATION_RESULT.ACCEPTABLE) return column + 1;
+            if (result == DURATION_RESULT.ACCEPTABLE) return column + 1;
 
             // if the result of the next column is now too long the current column is the most ideal
-            if(result == DURATION_RESULT.TOO_LONG || result == DURATION_RESULT.WITHIN_THIRTY_SECONDS_LONG){
+            if (result == DURATION_RESULT.TOO_LONG || result == DURATION_RESULT.WITHIN_THIRTY_SECONDS_LONG) {
                 return column;
-            }
-            else {
+            } else {
                 column++; // if the next column is not too long keep searching
             }
 
-        }while(true);
+        } while (true);
     }
 
     /**
      * Checks columns to the left of the given column in the TrackSimplified arrays in the intervals map for better
      * fitting columns
-     * @param intervals map to search the TrackSimplified arrays of
+     *
      * @param column column to start from
      * @return best fitting column
      */
-    private int checkForShorterColumn(HashMap<Integer, TrackSimplified[]> intervals, int column){
+    private int checkForShorterColumn(int column) {
 
         TrackSimplified[] next_column_tracks;
         DURATION_RESULT result;
 
-        do{
+        do {
 
             // if the next column is out of bounds return the current column
-            if(column - 1 < 0) return column;
+            if (column - 1 < 0) return column;
 
-            next_column_tracks = getTracksInColumn(intervals, column - 1);
+            next_column_tracks = getTracksInColumn(column - 1);
 
             result = checkDuration(next_column_tracks);
 
             // if the next column is a good fit, return it
-            if(result == DURATION_RESULT.ACCEPTABLE) return column + 1;
+            if (result == DURATION_RESULT.ACCEPTABLE) return column + 1;
 
             // if the result of the next column is now too short the current column is the most ideal
-            if(result == DURATION_RESULT.TOO_SHORT || result == DURATION_RESULT.WITHIN_THIRTY_SECONDS_SHORT){
+            if (result == DURATION_RESULT.TOO_SHORT || result == DURATION_RESULT.WITHIN_THIRTY_SECONDS_SHORT) {
                 return column;
-            }
-            else {
+            } else {
                 column--; // if the next column is not too long keep searching
             }
 
-        }while(true);
+        } while (true);
     }
 
     /**
      * Gets each track in the specified column from each interval in the provided intervals argument
      *
-     * @param intervals hashmap containing all the intervals and their tracks
-     * @param column    desired column to fetch a track from each interval
+     * @param column desired column to fetch a track from each interval
      * @return TrackSimplified array of the songs from the requested column in each interval
      */
-    private TrackSimplified[] getTracksInColumn(HashMap<Integer, TrackSimplified[]> intervals, int column) {
+    private TrackSimplified[] getTracksInColumn(int column) {
 
         TrackSimplified[] return_tracks = new TrackSimplified[num_intervals];
 
@@ -440,9 +507,7 @@ public class GenerateClassic extends GeneratePlaylist {
      */
     private String[] getWarmupTracks() throws Exception {
 
-        HashMap<Integer, TrackSimplified[]> sorted_intervals = new HashMap<>();
-
-        return findOrdering(sorted_intervals, true);
+        return findOrdering(true);
     }
 
     /**
@@ -453,9 +518,7 @@ public class GenerateClassic extends GeneratePlaylist {
      */
     private String[] getWindDownTracks() throws Exception {
 
-        HashMap<Integer, TrackSimplified[]> sorted_intervals = new HashMap<>();
-
-        return findOrdering(sorted_intervals, false);
+        return findOrdering(false);
     }
 
     /**
@@ -470,7 +533,7 @@ public class GenerateClassic extends GeneratePlaylist {
         String seed_artists = getSeedArtists();
         String seed_tracks = getSeedTracks();
 
-        HashMap<Integer, TrackSimplified[]> intervals = new HashMap<>();
+        HashMap<Integer, TrackSimplified[]> sorted_intervals = new HashMap<>();
 
         float query_bpm = initializeQueryBPM(is_warmup); // will be updated in for-loop
 
@@ -500,10 +563,10 @@ public class GenerateClassic extends GeneratePlaylist {
 
             Arrays.sort(recommended_tracks, duration_comparator);
 
-            intervals.put(current_interval, recommended_tracks);
+            sorted_intervals.put(current_interval, recommended_tracks);
         }
 
-        return intervals;
+        return sorted_intervals;
     }
 
     /**
@@ -539,9 +602,11 @@ public class GenerateClassic extends GeneratePlaylist {
      * Loops through all the given tracks and stores their IDs in a string array which is then returned
      *
      * @param tracks array of tracks to fetch the id from
-     * @return String array of all the given track's ids
+     * @return String array of all the given track's ids, returns null if tracks is null
      */
     private String[] getTrackIDs(TrackSimplified[] tracks) {
+
+        if (tracks == null) return null;
 
         String[] ids = new String[limit];
 
