b14620bec00cf8236163827b8746f1a36a97471a
Dalton-Simonson <dalton_simonson@brown.edu>
2023-07-18 17:17:59 -0400

    Got interval one playlist working but it needs more testing

--- back/src/main/java/PlaylistGenerating/PlaylistTypes/Interval/GenerateIntervalOne.java
+++ back/src/main/java/PlaylistGenerating/PlaylistTypes/Interval/GenerateIntervalOne.java
@@ -21,7 +21,7 @@ import static SpotifyUtilities.UserProfileUtilities.getCurrentUsersProfile;
  */
 public class GenerateIntervalOne extends GeneratePlaylist {
 
-    private final int num_intervals;
+    private int num_intervals;
     protected static int num_tracks = 0;
     protected static int target_length_ms = 0;
     protected static int min_target_len_ms;
@@ -33,13 +33,8 @@ public class GenerateIntervalOne extends GeneratePlaylist {
     protected static int num_slow_intervals = 0;
     protected static int num_fast_intervals = 0;
     protected static int og_offset = 3;
-
     protected static int num_slow_tracks;
     protected static int num_fast_tracks;
-
-    protected enum DURATION_RESULT {
-        ACCEPTABLE, TOO_SHORT, TOO_LONG, WITHIN_THIRTY_SECONDS_SHORT, WITHIN_THIRTY_SECONDS_LONG
-    }
 
     protected enum INTERVAL_TYPE {
         SLOW_INTERVAL, FAST_INTERVAL
@@ -59,57 +54,30 @@ public class GenerateIntervalOne extends GeneratePlaylist {
         super(spotify_api, genres, age, workout_length, intensity);
 
         num_intervals = getNumIntervals();
-        num_tracks = Math.round(workout_length_min / avg_song_len);
+
+        if(num_intervals % 2 == 0) num_intervals++; // Ensure we have an odd number of intervals
+
+        num_slow_intervals = (num_intervals / 2) + 1; // We want to start and end with a slow interval
+        num_fast_intervals = num_intervals - num_slow_intervals;
+
+        tracks_per_interval = getTracksPerInterval();
+        num_slow_tracks = num_slow_intervals * tracks_per_interval;
+        num_fast_tracks = num_fast_intervals * tracks_per_interval;
+
+        num_tracks = num_slow_tracks + num_fast_tracks;
+
 
         target_length_ms = workout_len_ms;
         setTargetLengths(margin_of_error);
 
-        // unsure if necessary?
-        tracks_per_interval = num_tracks / num_intervals;
-        num_slow_intervals = (num_intervals / 2) + 1; // We want to start and end with a slow interval
-        num_fast_intervals = num_intervals - num_slow_intervals;
-
-        num_slow_tracks = num_slow_intervals * tracks_per_interval;
-        num_fast_tracks = num_fast_tracks * tracks_per_interval;
-
-        //Todo: is this the right way to divide?
-        //Todo: should we keep using the same margin  of error for intervals?
         interval_len_ms = target_length_ms / num_intervals;
         setIntervalLengths(margin_of_error);
     }
 
-    /**
-     * Sets the min and max Target lengths based on the provided margin of error
-     *
-     * @param margin_of_error margin of error used in setting the min and max target lengths
-     */
-
-    /**
-     * Determine the number of intervals we need based on the length of the workout
-     *
-     * @return number of intervals we need
-     */
-    private int getNumIntervals() {
-        // Todo: test & adjust this function
-        // currently truncating anything after the decimal point... should we round up?
-
-        if (workout_length_min <= 30) {
-            // if workout is less than 30 minutes, do 1-song intervals
-            return (int) (workout_length_min / avg_song_len);
-        } else if (workout_length_min <= 60) {
-            // if workout is less than 60 minutes, do 2-song intervals
-            return (int) (workout_length_min / 2 * avg_song_len);
-        } else if (workout_length_min <= 120) {
-            // if workout is less than 120 minutes, do 3-song intervals
-            return (int) (workout_length_min / 3 * avg_song_len);
-        } else {
-            // if workout is more than 120 minutes, do 4-song intervals
-            return (int) (workout_length_min / 4 * avg_song_len);
-        }
-    }
-
     @Override
     public String generatePlaylist() throws Exception {
+
+        System.out.println("Generating Interval One Playlist");
 
         User user = getCurrentUsersProfile(spotify_api);
 
@@ -138,27 +106,25 @@ public class GenerateIntervalOne extends GeneratePlaylist {
      */
     private TrackSimplified[] buildPlaylist() throws GetRecommendationsException, GetAudioFeaturesForTrackException {
 
-        // Make an ArrayList to hold the final playlist
-        ArrayList<TrackSimplified> final_playlist = new ArrayList<>();
-
         ArrayList<TrackSimplified> recommended_slow_tracks;
         ArrayList<TrackSimplified> slow_intervals;
 
         ArrayList<TrackSimplified> recommended_fast_tracks;
         ArrayList<TrackSimplified> fast_intervals;
 
-        TrackSimplified[] best_fit_tracks = null;
+        TrackSimplified[] final_playlist = null;
 
-        float local_moe; // Keeps track of moe for duration purposes which we will be altering here
-
-        local_moe = margin_of_error;
+        // Keeps track of moe for duration purposes which we will be altering here
+        float local_moe = margin_of_error;
 
         do {
 
+            System.out.println("Getting Recommended Tracks");
             // Get recommended tracks (size of 100 each)
             recommended_slow_tracks = getRecommendedTracks(INTERVAL_TYPE.SLOW_INTERVAL);
             recommended_fast_tracks = getRecommendedTracks(INTERVAL_TYPE.FAST_INTERVAL);
 
+            System.out.println("Finding Rough Intervals");
             // Fill the intervals the best we can with the given 100 tracks
             slow_intervals = findRoughIntervals(recommended_slow_tracks, INTERVAL_TYPE.SLOW_INTERVAL);
             fast_intervals = findRoughIntervals(recommended_fast_tracks, INTERVAL_TYPE.FAST_INTERVAL);
@@ -166,21 +132,23 @@ public class GenerateIntervalOne extends GeneratePlaylist {
             // If enough of the intervals have been found, fill the gaps and sort them into one correctly ordered array
             if(slow_intervals != null && fast_intervals != null){
 
+                System.out.println("Filling Intervals");
                 // Find a good ordering of each interval
                 slow_intervals = fillIntervals(slow_intervals, INTERVAL_TYPE.SLOW_INTERVAL);
-                fast_intervals = fillIntervals(fast_intervals, INTERVAL_TYPE.SLOW_INTERVAL);
+                fast_intervals = fillIntervals(fast_intervals, INTERVAL_TYPE.FAST_INTERVAL);
 
+                System.out.println("Ordering Tracks");
                 // Order the tracks correctly
-                best_fit_tracks = null;
+                final_playlist = orderTracks(slow_intervals, fast_intervals);
             }
 
             setIntervalLengths(local_moe += .01); // loosen MOE
 
-        } while (best_fit_tracks == null);
+        } while (final_playlist == null);
 
         setIntervalLengths(margin_of_error); // restore moe
 
-        return best_fit_tracks;
+        return final_playlist;
     }
 
     /**
@@ -194,6 +162,7 @@ public class GenerateIntervalOne extends GeneratePlaylist {
 
         //int local_offset = og_offset; //currently 3
         int limit = 100; // 100 is the max for recommendation endpoint
+        int local_offset = og_offset;
 
         do {
 
@@ -201,17 +170,20 @@ public class GenerateIntervalOne extends GeneratePlaylist {
 
             if (interval_type == INTERVAL_TYPE.SLOW_INTERVAL) {
                 recommended_tracks = getSortedRecommendations(limit,
-                        resting_bpm - og_offset, resting_bpm + og_offset, resting_bpm);
+                        resting_bpm - local_offset, resting_bpm + local_offset, resting_bpm);
             } else {
                 recommended_tracks = getSortedRecommendations(limit,
-                        target_bpm - og_offset, target_bpm + og_offset, target_bpm);
+                        target_bpm - local_offset, target_bpm + local_offset, target_bpm);
             }
 
-            if (recommended_tracks == null || recommended_tracks.length < limit) {
-                og_offset++;
+
+            //TODO: look at this 90 and see if another value is better
+            if (recommended_tracks == null || recommended_tracks.length < 90) {
+                local_offset++;
                 continue;
             }
 
+            System.out.println("Recommended Size: " + recommended_tracks.length);
             return new ArrayList<>(Arrays.asList(recommended_tracks));
 
         } while (true);
@@ -231,8 +203,8 @@ public class GenerateIntervalOne extends GeneratePlaylist {
         Deque<TrackSimplified> deque = new ArrayDeque<>();
         ArrayList<TrackSimplified> selected_tracks = new ArrayList<>();
         TrackSimplified current_track;
-        TrackSimplified current_fast_track;
         DURATION_RESULT result;
+        float acceptable_percent_filled = .75f;
 
         int index = 0;
 
@@ -254,17 +226,20 @@ public class GenerateIntervalOne extends GeneratePlaylist {
             intervals_to_fill = num_fast_intervals;
         }
 
+        // Pick up where we left off
         for (; index < track_pool.size() ; index++) {
 
             result = checkIntervalDuration(deque);
 
             if(result == DURATION_RESULT.ACCEPTABLE){
 
+                int deque_size = deque.size(); // Store size here as in the loop size will be altered by .pop()
+
                 // Empty the deque, add tracks to the selected tracks, remove songs from track pool
-                for(int i = 0; i < deque.size(); i++){
+                for(int i = 0; i < deque_size; i++){
                     current_track = deque.pop();
 
-                    selected_tracks.add(current_track); // Place it in our collection of slow tracks
+                    selected_tracks.add(current_track); // Place it in our collection of selected tracks
                     track_pool.remove(current_track); // Removing to avoid having these added later as dupes
                 }
 
@@ -281,22 +256,27 @@ public class GenerateIntervalOne extends GeneratePlaylist {
                     deque.add(current_track);
                 }
 
-                continue; // restart loop
-            } else if (result == DURATION_RESULT.TOO_LONG) {
+            }
+            // If the combination is too long there are no combinations remaining that will fit into our intervals
+            else if (result == DURATION_RESULT.TOO_LONG) {
                 break;
             }
+            else {
 
-            // If duration was not acceptable shift tracks over to find better length
-            current_track = track_pool.get(index);
+                // If duration was not acceptable shift tracks over to find better length
+                current_track = track_pool.get(index);
 
-            // This essentially shifts the group of tracks we are analyzing to the right (longer) side
-            deque.removeFirst(); // remove the shortest track
-            deque.add(current_track); // add the next track in line
+                // This essentially shifts the group of tracks we are analyzing to the right (longer) side
+                deque.removeFirst(); // remove the shortest track
+                deque.add(current_track); // add the next track in line
+            }
         }
 
-        // TODO: find good percentage breakpoint
-        // If less than ??75%?? of intervals were filled return null and try again with looser margins
-        if( ((float)intervals_filled / intervals_to_fill) < .75f) return null;
+        // If we did not fill enough of the intervals return null, so we can try again with looser margins
+        float percent_filled = ((float)intervals_filled / intervals_to_fill) * 100;
+        System.out.println("Percent Filled: " + percent_filled + "%");
+
+        if( percent_filled < acceptable_percent_filled) return null;
 
         return selected_tracks;
     }
@@ -312,31 +292,38 @@ public class GenerateIntervalOne extends GeneratePlaylist {
     ArrayList<TrackSimplified> fillIntervals(ArrayList<TrackSimplified> tracks,
                                              INTERVAL_TYPE interval_type) throws GetRecommendationsException {
 
-
         TrackSimplified[] recommended_tracks;
         TrackSimplified[] tracks_to_add = null;
         int num_tracks_needed;
+        int query_bpm;
 
-        float local_moe = margin_of_error; // Keeps track of moe for duration purposes which we will be altering here
+        float local_moe; // Keeps track of moe for duration purposes which we will be altering here
         int limit = 21;
-        int local_offset = 3;
 
+        // Find how many tracks we need to find and at what bpm we should find tracks
         if(interval_type == INTERVAL_TYPE.SLOW_INTERVAL){
             num_tracks_needed = num_slow_tracks - tracks.size();
+            query_bpm = resting_bpm;
         }else{
             num_tracks_needed = num_fast_tracks - tracks.size();
+            query_bpm = target_bpm;
         }
+
+        System.out.println("Tracks Needed: " + num_tracks_needed);
 
         for(int i = 0; i < num_tracks_needed; i++){
 
+            local_moe = margin_of_error;
+
             do {
 
-                recommended_tracks = getRecommendedTracks(limit, resting_bpm - local_offset,
-                        resting_bpm + local_offset, resting_bpm);
+                recommended_tracks = getRecommendedTracks(limit, query_bpm - og_offset,
+                        query_bpm + og_offset, query_bpm);
 
                 tracks_to_add = getIntervalTracks(recommended_tracks);
 
-                setIntervalLengths(local_moe += .01);
+                //TODO: find good increase to local_moe
+                setIntervalLengths(local_moe += .005);
 
             } while (tracks_to_add == null);
 
@@ -350,14 +337,37 @@ public class GenerateIntervalOne extends GeneratePlaylist {
 
     /**
      * Orders the slow and fast tracks into the correct ordering desired for the final playlist
-     * @param slow_tracks
-     * @param fast_tracks
+     * @param slow_tracks tracks in the slow interval
+     * @param fast_tracks tracks in the fast interval
      * @return
      */
     private TrackSimplified[] orderTracks(ArrayList<TrackSimplified> slow_tracks,
                                           ArrayList<TrackSimplified> fast_tracks){
 
-        return null;
+        TrackSimplified[] ordered_playlist = new TrackSimplified[num_tracks];
+        ArrayList<TrackSimplified> current_tracks = slow_tracks;
+        boolean is_slow_interval = true; // Every playlist will start with a slow interval
+
+        for(int index = 0; index < num_tracks; index++){
+
+            // If we have added enough tracks for the current interval, ignore first check when index is 0
+            if( index % tracks_per_interval == 0 && index != 0){
+
+                if(is_slow_interval){
+                    current_tracks = fast_tracks;
+
+                }else{
+                    current_tracks = slow_tracks;
+                }
+
+                is_slow_interval = !is_slow_interval;
+            }
+
+            ordered_playlist[index] = current_tracks.remove(0);
+
+        }
+
+        return ordered_playlist;
     }
 
 
@@ -399,6 +409,53 @@ public class GenerateIntervalOne extends GeneratePlaylist {
         return null;
     }
 
+    /**
+     * Sets the min and max Target lengths based on the provided margin of error
+     *
+     * @param margin_of_error margin of error used in setting the min and max target lengths
+     */
+
+    /**
+     * Determine the number of intervals we need based on the length of the workout
+     *
+     * @return number of intervals we need
+     */
+    private int getNumIntervals() {
+        // Todo: test & adjust this function
+        // currently truncating anything after the decimal point... should we round up?
+
+        if (workout_length_min <= 30) {
+            // if workout is less than 30 minutes, do 1-song intervals
+            return (int) (workout_length_min / avg_song_len);
+        } else if (workout_length_min <= 60) {
+            // if workout is less than 60 minutes, do 2-song intervals
+            return (int) (workout_length_min / (2 * avg_song_len));
+        } else if (workout_length_min <= 120) {
+            // if workout is less than 120 minutes, do 3-song intervals
+            return (int) (workout_length_min / (3 * avg_song_len));
+        } else {
+            // if workout is more than 120 minutes, do 4-song intervals
+            return (int) (workout_length_min / (4 * avg_song_len));
+        }
+    }
+
+    private int getTracksPerInterval(){
+        if (workout_length_min <= 30) {
+            // if workout is less than 30 minutes, do 1-song intervals
+            return 1;
+        } else if (workout_length_min <= 60) {
+            // if workout is less than 60 minutes, do 2-song intervals
+            return 2;
+        } else if (workout_length_min <= 120) {
+            // if workout is less than 120 minutes, do 3-song intervals
+            return 3;
+        } else {
+            // if workout is more than 120 minutes, do 4-song intervals
+            return 4;
+        }
+    }
+
+
     private void setTargetLengths(float margin_of_error) {
         // MilliSeconds of length that is acceptable for the target sequence based on our MOE
         min_target_len_ms = target_length_ms - (int) (target_length_ms * margin_of_error);
@@ -430,10 +487,11 @@ public class GenerateIntervalOne extends GeneratePlaylist {
             duration_ms += track.getDurationMs();
         }
 
-//        System.out.println("Duration: " + duration_ms);
-//        System.out.println("Interval Duration: " + interval_len_ms);
-//        System.out.println("Min Duration: " + min_interval_len_ms);
-//        System.out.println("Max Duration: " + max_interval_len_ms);
+        System.out.println("Duration: " + duration_ms);
+        System.out.println("Interval Duration: " + interval_len_ms);
+        System.out.println("Min Duration: " + min_interval_len_ms);
+        System.out.println("Max Duration: " + max_interval_len_ms);
+        System.out.println();
 
 
         if (duration_ms < min_interval_len_ms) {
@@ -467,7 +525,7 @@ public class GenerateIntervalOne extends GeneratePlaylist {
 
             local_offset++; // increase offset to find more tracks as the current bpm boundaries may be too restrictive
 
-        } while (recommended_tracks.length < limit);
+        } while (recommended_tracks == null || recommended_tracks.length < limit);
 
         return recommended_tracks;
     }
